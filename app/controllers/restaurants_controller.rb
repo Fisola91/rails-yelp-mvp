12ec12a0f71fca93f09063f469f4ca2e20870bfb
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-02-17 00:38:19 +0100

    Rails Yelp MVP

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,35 @@
+class RestaurantsController < ApplicationController
+
+  # GET /restaurants
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  # GET restaurants/1
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @review = Review.new
+  end
+
+  # GET /restaurants/new
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  # POST / restaurants
+  def create
+    # Add a new restaurant
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database"
+Restaurant.destroy_all
+
+puts "Creating restaurants.."
+
+Restaurant.create!(name: "Max und Moritz", address: "Oranienstraße", phone_number: "03000222558888", category: "french")
+
+Restaurant.create!(name: "Tim Raue", address: "Oranienstraße", phone_number: "03000222558888", category: "chinese")
+
+Restaurant.create!(name: "Max und Moritz", address: "Oranienstraße", phone_number: "03000222558888", category: "italian")
+
+Restaurant.create!(name: "Salad", address: "Oranienstraße", phone_number: "03000222558888", category: "japanese")
+
+Restaurant.create!(name: "Rice", address: "Oranienstraße", phone_number: "03000222558888", category: "belgian")
